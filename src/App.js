6eb3f0d4bafd2f42f6d92b0e4433087cfee6cdd0
Aditya Dubey <dubeyadi8602@gmail.com>
2021-04-18 19:50:31 +0530

    Header Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from "./components/Navbar"
+import Particles from 'react-particles-js';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 function App() {
   return (
+    <>
+    <Particles
+    className = "particles-canvas"
+      params = {{
+        particles: {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area: 900
+            }
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 7,
+              color: "f9ab00"
+            }
+          }
+        }
+      }}
+    />
     <Navbar />
+    <Header />
+    </>
   );
 }
 
