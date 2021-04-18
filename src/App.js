1d239f104193f7513f17f95c3f728b7c6e84a926
Aditya Dubey <dubeyadi8602@gmail.com>
2021-04-18 17:12:59 +0530

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/Navbar"
 function App() {
   return (
-    <h2>Portfolio Project....</h2>
+    <Navbar />
   );
 }
 
